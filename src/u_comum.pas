06b4facafea924d39a050d1773720057d5662736
Vico <vpontin93@gmail.com>
2023-06-01 13:35:45 -0300

    Adicionada função pra pegar pasta padrão
    No Windows, Meus Documentos
    Nos outros, a pasta do usuário

--- src/u_comum.pas
+++ src/u_comum.pas
@@ -5,15 +5,25 @@ unit u_comum;
 interface
 
 uses
-  Forms, Classes, SysUtils;
+  Forms, Classes, SysUtils {$IFDEF MSWINDOWS}, windirs {$ENDIF};
 
 procedure sair_programa;
+function get_default_dir: string;
 
 implementation
 
 procedure sair_programa;
 begin
   Application.Terminate;
+end;
+
+function get_default_dir: string;
+begin
+  {$IFDEF MSWINDOWS}
+    Result:=GetWindowsSpecialDir(CSIDL_PERSONAL);
+  {$ELSE}
+    Result := GetUserDir;
+  {$ENDIF}
 end;
 
 end.
